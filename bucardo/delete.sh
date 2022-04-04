c76f958b70204137737868f8b83a3b53530faceb
Manuel Jesús Parra Royón <manuparra@gmail.com>
2022-04-04 12:59:29 +0200

    New delete service added

---
+++ bucardo/delete.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+RED="31"
+GREEN="32"
+YELLOW="33"
+BOLDGREEN="\e[1;${GREEN}m"
+BOLDYELLOW="\e[1;${YELLOW}m"
+ITALICGREEN="\e[3;${GREEN}m"
+ENDCOLOR="\e[0m"
+
+cd $HOME/bucardo/
+echo -e "${BOLDGREEN}Clean-up latest version of the Bucardo replication data scheme${ENDCOLOR}"
+bucardo restart
