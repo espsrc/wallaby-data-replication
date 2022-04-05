66f400aba6db01a333f3a426761c9711cf28268e
Manuel Parra <manuparra@gmail.com>
2022-04-05 18:45:51 +0200

    New sequence file

--- bucardo/scripts/cleanup.sh
+++ bucardo/scripts/cleanup.sh
@@ -14,6 +14,10 @@ bucardo delete tables all --batch --quiet
 bucardo delete sequences all --batch --quiet
 bucardo delete syncs wallabydb_aus_dbsync
 bucardo delete syncs wallabydb_cirada_dbsync
+bucardo delete syncs wallabydb_sequences_aussrc
+bucardo delete syncs wallabydb_sequences_cirada
+bucardo delete relgroups seq_aussrc
+bucardo delete relgroups seq_ciradac
 bucardo delete relgroups aussrctables
 bucardo delete relgroups ciradatables
 bucardo delete database wallabydb_aussrc
