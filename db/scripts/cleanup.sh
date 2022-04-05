21be13cf4ce8fb929f37ab82f74c9badcd1f3e10
Manuel Parra <manuparra@gmail.com>
2022-04-05 09:27:09 +0200

    Cleaning up DB and Connection to the DB.

---
+++ db/scripts/cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RED="31"
+GREEN="32"
+YELLOW="33"
+BOLDGREEN="\e[1;${GREEN}m"
+BOLDYELLOW="\e[1;${YELLOW}m"
+ITALICGREEN="\e[3;${GREEN}m"
+ENDCOLOR="\e[0m"
+
+echo -e "${BOLDYELLOW}Removing connections to the database${ENDCOLOR}"
+sudo su - postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = 'wallabydb';"
+echo -e "${BOLDYELLOW}Removing database wallabydb${ENDCOLOR}"
+sudo su - postgres -c "psql -U postgres -c 'DROP DATABASE wallabydb;'"
