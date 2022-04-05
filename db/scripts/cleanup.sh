72ce8dcacab95f0e6ec76e6e3da550e4c52d7428
Manuel Parra <manuparra@gmail.com>
2022-04-05 09:30:01 +0200

    datname - changed

--- db/scripts/cleanup.sh
+++ db/scripts/cleanup.sh
@@ -9,6 +9,6 @@ ITALICGREEN="\e[3;${GREEN}m"
 ENDCOLOR="\e[0m"
 
 echo -e "${BOLDYELLOW}Removing connections to the database${ENDCOLOR}"
-sudo su - postgres -c "psql -U postgres -c 'SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = 'wallabydb';'"
+sudo su - postgres -c "psql -U postgres -c 'SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = 'wallaby';'"
 echo -e "${BOLDYELLOW}Removing database wallabydb${ENDCOLOR}"
 sudo su - postgres -c "psql -U postgres -c 'DROP DATABASE wallabydb;'"
