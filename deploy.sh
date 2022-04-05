c501a65759c0e4cfbb9d6e56eeb3f4acb7b01ba6
Manuel Parra <manuparra@gmail.com>
2022-04-05 12:19:03 +0200

    Argument to add dev env based on fork

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,8 @@ cd $HOME/bucardo/
 # Creating (wipe-out db and re-create db) WALLABYDB
 echo -e "${BOLDGREEN}Creating DB${ENDCOLOR}"
 echo -e "${BOLDGREEN}-------------------------------------------${ENDCOLOR}"
-bash wallaby-data-replication/db/create.sh
+if [ $1 == "--dev" ]; then bash wallaby-data-replication/db/create-dev.sh; fi
+if [ $1 == "" ]; then      bash wallaby-data-replication/db/create.sh; fi
 echo -e "${BOLDGREEN}Creating BUCARDO SYNC${ENDCOLOR}"
 echo -e "${BOLDGREEN}-------------------------------------------${ENDCOLOR}"
 bash wallaby-data-replication/bucardo/delete.sh
